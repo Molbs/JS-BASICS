639685199d18530720e1c825d3f3551efa9b9828
Molbs <molbstech@gmail.com>
2023-01-12 12:18:41 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// let name = 'Moses';
+// declaring an object
+let person = {
+    name:'Nielsen',
+    age:23,
+    spouse:'Maria',
+    address:{
+        city:'Nairobi',
+        po_Box: '3764_citySquare',
+        postal_code: 00200
+    }
+}
+console.log(person);
