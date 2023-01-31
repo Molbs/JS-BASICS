d4c06328289253bd152cb00876e87d0572688e9d
Molbs <moses.alukwe@sgnewtech.com>
2023-01-31 10:26:18 +0300

    update

--- pirple1.js
+++ pirple1.js
@@ -101,6 +101,11 @@ const users = [
     {name:'nielsen', age:25},
     {name:'alukwe', age:36}
 ];
+const clubs = [
+    {name:'Man United', rank:1},
+    {name:'Man city', rank:2},
+    {name:'Arsenal', rank:3}
+];
 const userName = users.map( (user) =>{
     return user.name;
 })
@@ -117,3 +122,84 @@ console.log(mapUser(users));
 // skip bracket when one expression is expected example
 const multiplies = (num1,num2) => num1*num2;
 console.log(multiplies(4,5));
+// skip bracket
+const mapUsers = (myArray) => 
+myArray.map( (user) => user.name);
+
+console.log(mapUsers(clubs));
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const numbersFiltered = numbers.filter((num) => num % 2 !=0);
+console.log(numbersFiltered);
+// updating a list with a doubled numbers using a function
+const doubleNumbers = (num) => {
+    const numberContainer = [];
+    num.forEach((n) => {
+        const doubled = n*2;
+        numberContainer.push(doubled);
+        
+    });
+    return numberContainer
+}
+console.log(...doubleNumbers(numbers));
+
+// assigning default values 
+const numbMultiplier = (num1, numb2) => {
+    num1 = num1 || 5;
+    numb2 = numb2 || 15;
+    
+    return num1 * numb2;
+}
+console.log(numbMultiplier());
+console.log(numbMultiplier(4,5));
+
+// assign 2
+const numberMulti = (num1 = 12, num2 = 6) => num1 * num2;
+console.log(numberMulti());
+
+// splitting strings - returns an array with the slipt words
+function stringSplitter(str){
+    return str.split(",");
+
+}
+console.log(stringSplitter("hello,My name is Moses."));
+// alternative
+const textSlipt = (str) => str.split(" ");
+console.log(textSlipt("hello Moses"));
+// purchasing
+const bankAccount = {
+    canSpend : true,
+    hasCredit: true,
+    balance: 2700
+};
+function purchase(price,account = bankAccount){
+    if(account.canSpend){
+        if(account.hasCredit){
+            if(account.balance >= price){
+                account.balance -= price;
+                if(account.balance <= 0){
+                    account.canSpend = false;
+                }
+                
+            }
+            else{
+                return false
+            }
+            return true
+
+        }
+       
+        else{
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
+}
+console.log(purchase(27000));
+console.log(bankAccount);
+console.log(purchase(2000));
+console.log(bankAccount);
+console.log(purchase(700));
+console.log(bankAccount);
+console.log(purchase(700));
